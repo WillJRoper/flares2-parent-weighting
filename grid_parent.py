e0727889e111e3e30ecef9b68a09fbf4622e5af5
wjr21 <wjr21@sussex.ac.uk>
2021-09-07 17:19:25 +0200

    added various different griddings

--- grid_parent.py
+++ grid_parent.py
@@ -5,58 +5,97 @@ import h5py
 filepath = "/cosma8/data/dp004/jlvc76/FLAMINGO/ScienceRuns/" \
            "DMO/L3200N5760/snapshots/flamingo_0000/flamingo_0000.hdf5"
 
-# Open HDF5 file
-hdf = h5py.File(filepath, "r")
+def get_and_write_ovdengrid(filepath, zoom_ncells, zoom_width):
 
-# Get metadata
-boxsize = hdf["Header"].attrs["BoxSize"]
-z = hdf["Header"].attrs["Redshift"]
-dm_mass = hdf["PartType1/Masses"][0]
+    # Open HDF5 file
+    hdf = h5py.File(filepath, "r")
 
-# Read in the cell centres and size
-nr_cells = int(hdf["/Cells/Meta-data"].attrs["nr_cells"])
-centres = hdf["/Cells/Centres"][:, :]
+    # Get metadata
+    boxsize = hdf["Header"].attrs["BoxSize"]
+    z = hdf["Header"].attrs["Redshift"]
+    nparts = hdf["Header"].attrs["NumPart_Total"]
+    dm_npart = nparts[1]
+    dm_mass = hdf["PartType1/Masses"][0]
 
-# Set up overdensity grid array
+    # Read in the cell centres and size
+    nr_cells = int(hdf["/Cells/Meta-data"].attrs["nr_cells"])
+
+    # Set up overdensity grid array
+    cell_width = zoom_width / zoom_ncells
+    ncells = np.int32(boxsize / cell_width)
+    cell_volume = cell_width**3
+
+    mass_grid = np.zeros(ncells)
+
+    mean_density = dm_npart * dm_mass / boxsize**3
+
+    print("N_part:", nparts)
+    print("Boxsize:", boxsize)
+    print("Redshift:", z)
+    print("Cell Width:", cell_width)
+    print("nr_cells:", nr_cells)
+    print("grid cells:", ncells)
+
+    for icell in range(nr_cells):
+
+        print(icell, "of", nr_cells)
+
+        # Retrieve the offset and counts
+        my_offset = hdf["/Cells/OffsetsInFile/PartType1"][icell]
+        my_count = hdf["/Cells/Counts/PartType1"][icell]
+
+        # Get the densities of the particles in this cell
+        poss = hdf["/PartType1/Coordinates"][my_offset: my_offset + my_count]
+
+        for ipart in range(poss.shape[0]):
+
+            this_pos = poss[ipart, :]
+
+            i = int(this_pos[0] / cell_width)
+            j = int(this_pos[1] / cell_width)
+            k = int(this_pos[2] / cell_width)
+
+            mass_grid[i, j, k] += dm_mass
+
+    hdf.close()
+
+    # Convert mass grid to overdensities
+    den_grid = mass_grid / cell_volume
+    ovden_grid = (den_grid - mean_density) / mean_density
+
+    try:
+        hdf = h5py.File("data/parent_ovden_grid.hdf5", "r+")
+        grid_grp = hdf["Grids"]
+    except OSError as e:
+        print(e)
+        hdf = h5py.File("data/parent_ovden_grid.hdf5", "w")
+        grid_grp = hdf.create_group("Grids")
+
+    try:
+        zoom_grp = grid_grp.create_group(str(zoom_width) + "_"
+                                         + str(zoom_ncells))
+    except OSError as e:
+        print(e)
+        del grid_grp[str(zoom_width) + "_" + str(zoom_ncells)]
+        zoom_grp = grid_grp.create_group(str(zoom_width) + "_"
+                                         + str(zoom_ncells))
+
+    zoom_grp.attrs["zoom_ncells"] = zoom_ncells
+    zoom_grp.attrs["zoom_width"] = zoom_width
+    zoom_grp.attrs["cell_width"] = cell_width
+    zoom_grp.create_dataset("Mass_grid", data=mass_grid)
+    zoom_grp.create_dataset("Density_grid", data=den_grid)
+    zoom_grp.create_dataset("Overdensity_grid", data=ovden_grid)
+
+    hdf.close()
+
 zoom_width = 25
-zoom_ncells = 32
-cell_width = zoom_width / zoom_ncells
-ncells = np.int32(boxsize / cell_width)
+for zoom_ncells in [32, 64, 128, 256]:
+    print("================== zoom width, zoom_ncells =",
+          zoom_width, zoom_ncells, "==================")
+    get_and_write_ovdengrid(filepath, zoom_ncells, zoom_width)
 
-cell_volume = cell_width**3
 
-mass_grid = np.zeros(ncells)
-
-print("Boxsize:", boxsize)
-print("Redshift:", z)
-
-for icell in range(nr_cells):
-
-    # Print the position of the centre of the cell of interest
-    centre = hdf["/Cells/Centres"][icell, :]
-    print("Centre of the cell:", centre)
-
-    # Retrieve the offset and counts
-    my_offset = hdf["/Cells/OffsetsInFile/PartType1"][icell]
-    my_count = hdf["/Cells/Counts/PartType1"][icell]
-
-    # Get the densities of the particles in this cell
-    poss = hdf["/PartType1/Coordinates"][my_offset: my_offset + my_count]
-
-    for ipart in range(poss.shape[0]):
-
-        this_pos = poss[ipart, :]
-
-        i = int(this_pos[0] / cell_width)
-        j = int(this_pos[1] / cell_width)
-        k = int(this_pos[2] / cell_width)
-
-        mass_grid[i, j, k] += dm_mass
-
-hdf.close()
-
-# Convert mass grid to overdensities
-ovden_grid = mass_grid / cell_width * 10**10
 
 
 
