dcb5d572eb0898f74ee7cd26ca2e39df8cc66216
wjr21 <wjr21@sussex.ac.uk>
2021-09-10 17:05:33 +0200

    added timing

--- get_mass_grid.sh
+++ get_mass_grid.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -l
-#SBATCH --ntasks 1 # The number of cores you need...
-#SBATCH --array=1-300%4
-#SBATCH --ntasks-per-node=96
-#SBATCH -p cosma8 #or some other partition, e.g. cosma, cosma6, etc.
+#SBATCH --ntasks 128
+#SBATCH -N 1
+#SBATCH -J Grid-WITH-PERMISSION
+#SBATCH -o logs/standard_output_file.%J.out
+#SBATCH -e logs/standard_error_file.%J.err
+#SBATCH -p cosma8
 #SBATCH -A dp004
-#SBATCH --cpus-per-task=1
-#SBATCH -J Grid-WITH-PERMISSION #Give it something meaningful.
-#SBATCH -o logs/output_hlr_job.%A_%a.out
-#SBATCH -t 72:00:00
-
-cd /cosma/home/dp004/dc-rope1/FLARES/FLARES-2-codes/flares2-parent-weighting
+#SBATCH --exclusive
+#SBATCH -t 3:00:00
 
 module purge
 #load the modules used to build your program.
+module load gnu_comp/7.3.0
+module load openmpi/3.0.1
 module load pythonconda3/4.5.4
+
+cd /cosma/home/dp004/dc-rope1/FLARES/FLARES-2-codes/flares2-parent-weighting
 
 source activate flares-env
 
-i=$(($SLURM_ARRAY_TASK_ID - 1))
-
-# Run the program
-python grid_parent_distributed.py $i 100
+# Run the program 200 times (on 128 cores).
+mpirun -np 128 /cosma/home/dp004/dc-rope1/parallel_tasks/build/parallel_tasks 0 200 "python ./grid_parent_distributed.py %d 200"
 
 source deactivate
 
