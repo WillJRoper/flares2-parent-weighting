5659c8c398a53980f4d4cb56bfcdefbfe45960ff
wjr21 <wjr21@sussex.ac.uk>
2021-09-10 12:48:01 +0200

    reduced write out

--- get_mass_grid.sh
+++ get_mass_grid.sh
@@ -4,9 +4,8 @@
 #SBATCH -p cosma8 #or some other partition, e.g. cosma, cosma6, etc.
 #SBATCH -A dp004
 #SBATCH --cpus-per-task=1
-#SBATCH -J FLARES-2-Grid #Give it something meaningful.
+#SBATCH -J Grid-WITH-PERMISSION #Give it something meaningful.
 #SBATCH -o logs/output_hlr_job.%A_%a.out
-#SBATCH -e logs/error_hlr_job.%A_%a.err
 #SBATCH -t 72:00:00
 
 cd /cosma/home/dp004/dc-rope1/FLARES/FLARES-2-codes/flares2-parent-weighting
@@ -25,6 +24,8 @@ python grid_parent_distributed.py $i 100
 source deactivate
 
 echo "Job done, info follows..."
-sacct -j $SLURM_JOBID --format=JobID,JobName,Partition,MaxRSS,Elapsed,ExitCode
+sstat --jobs=${SLURM_JOBID}.batch --format=JobID,MaxRSS,AveCPU,AvePages,AveRSS,AveVMSize
 exit
+
+
 
