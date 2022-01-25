27672e6c4793492014e385f1731084ac60e22b35
wjr21 <wjr21@sussex.ac.uk>
2022-01-25 22:54:53 +0000

    added cell edges to file

--- grid_parent_distributed.py
+++ grid_parent_distributed.py
@@ -184,7 +184,7 @@ def create_meta_file(metafile, rankfile_dir, outfile_without_rank, size):
         for key in hdf_rank.keys():
             i, j, k = key.split("_")
             cid = get_cellid(cdim, int(i), int(j) , int(k))
-            hdf_meta[cid] = h5py.ExternalLink(rankfile, key)
+            hdf_meta[str(cid)] = h5py.ExternalLink(rankfile, key)
 
         hdf_rank.close()
 
