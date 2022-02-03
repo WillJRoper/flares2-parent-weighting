e12aefc8c6d127971dd6efd091ea99f339c20e9a
wjr21 <wjr21@sussex.ac.uk>
2022-02-03 15:07:25 +0000

    added scripts for region stats

--- hist_region_multi_z.py
+++ hist_region_multi_z.py
@@ -19,7 +19,10 @@ ini_kernel_width = int(sys.argv[3])
 
 
 # Extract the snapshot string
-snaps = [str(i).zfill(4) for i in range(0, 20)]
+snaps = [str(i).zfill(4) for i in range(0, 21)]
+zs = [15, 12.26, 10.38, 9.51, 8.7, 7.95, 7.26, 6.63, 6.04, 5.50, 5.0, 4.75,
+      4.5, 4.25, 2.0, 3.75, 3.5, 3.25, 3.0, 2.95, 2.90, 2.85, 2.8, 2.75,
+      2.7, 2.65, 2.6, 2.55, 2.5]
 
 # Set up bins
 step = 0.1
@@ -37,7 +40,7 @@ fig_log = plt.figure()
 ax_log = fig_log.add_subplot(111)
 ax_log.semilogy()
 
-for snap in snaps:
+for num, snap in enumerate(snaps):
 
        # Define output paths
        metafile = "overdensity_" + sim_tag + "_" + sim_type + "_snap%s.hdf5" % snap
@@ -51,7 +54,7 @@ for snap in snaps:
        # Open file
        hdf = h5py.File(path, "r")
 
-       z = hdf["Parent"].attrs["Redshift"]
+       z = [num]
        grid = hdf["Region_Overdensity"][...]
 
        hdf.close()
