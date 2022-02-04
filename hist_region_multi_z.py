e0ebed52e29a005fe96a31eb14f08184481c7259
wjr21 <wjr21@sussex.ac.uk>
2022-02-04 18:01:24 +0000

    region selection script and region smoothing now produces sorting indices to aide selection

--- hist_region_multi_z.py
+++ hist_region_multi_z.py
@@ -5,6 +5,8 @@ import matplotlib as mpl
 from flare import plt as flareplt
 import h5py
 import sys
+
+
 plt.rcParams['axes.grid'] = True
 
 
@@ -17,12 +19,8 @@ sim_type = sys.argv[2]
 # Define kernel width
 ini_kernel_width = int(sys.argv[3])
 
-
 # Extract the snapshot string
 snaps = [str(i).zfill(4) for i in range(0, 20)]
-zs = [15, 12.26, 10.38, 9.51, 8.7, 7.95, 7.26, 6.63, 6.04, 5.50, 5.0, 4.75,
-      4.5, 4.25, 2.0, 3.75, 3.5, 3.25, 3.0, 2.95, 2.90, 2.85, 2.8, 2.75,
-      2.7, 2.65, 2.6, 2.55, 2.5]
 
 # Set up bins
 step = 0.1
