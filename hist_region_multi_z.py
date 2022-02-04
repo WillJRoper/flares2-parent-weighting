5cc3db3b7b13a408c58346df597fc1c5eefa4654
wjr21 <wjr21@sussex.ac.uk>
2022-02-04 23:54:04 +0000

    region selection script and region smoothing now produces sorting indices to aide selection

--- hist_region_multi_z.py
+++ hist_region_multi_z.py
@@ -71,6 +71,7 @@ ax_log.set_ylabel("$N$")
 ax2_log = fig_log.add_axes([0.95, 0.1, 0.015, 0.8])
 cb1 = mpl.colorbar.ColorbarBase(ax2_log, cmap=cmap,
                                 norm=norm)
+cb1.set_label("$z$")
 
 fig_log.savefig("plots/log_region_multiz_" + str(ini_kernel_width) + "_"  + sim_tag + "_" + sim_type + ".png",
                 bbox_inches="tight")
